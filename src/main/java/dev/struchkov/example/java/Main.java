06a1c5f4a4ffb4b1d27809ac97728b7034c9fb25
Struchkov Mark <git@upagge.ru>
2021-12-04 10:21:02 +0300

    fix

--- src/main/java/dev/struchkov/example/java/Main.java
+++ src/main/java/dev/struchkov/example/java/Main.java
@@ -3,6 +3,15 @@ package dev.struchkov.example.java;
 public class Main {
 
     public static void main(String[] args) {
+        java8();
+        java9();
+    }
+
+    public static void java8() {
+
+    }
+
+    public static void java9() {
 
     }
 
